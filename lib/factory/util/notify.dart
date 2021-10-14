c7d641569ae7d382d3b6530c3ecc902a673e3ffb
unkonown <b1021091@fun.ac.jp>
2021-10-15 03:03:24 +0900

    fixing notify

--- lib/factory/util/notify.dart
+++ lib/factory/util/notify.dart
@@ -3,11 +3,11 @@ import 'package:flutter/foundation.dart';
 import 'package:matcha/factory/util/date.dart';
 
 createNotification({
-  required String title,
-  required String body,
-  required int id,
+  required String? title,
+  required String? body,
+  required int? id,
   required String date,
-  required String channelKey,
+  required String? channelKey,
   int days = 0,
   int hours = 0,
   int minutes = 0,
