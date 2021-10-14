d527ecb335db036931b762cf62f1917efb0b24e0
unkonown <b1021091@fun.ac.jp>
2021-10-15 02:35:26 +0900

    fixing notify

--- lib/factory/util/notify.dart
+++ lib/factory/util/notify.dart
@@ -2,7 +2,7 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/foundation.dart';
 import 'package:matcha/factory/util/date.dart';
 
-void createNotification({
+createNotification({
   required String title,
   required String body,
   required int id,
@@ -12,19 +12,17 @@ void createNotification({
   int hours = 0,
   int minutes = 0,
   int seconds = 0,
-}) async {
-  print('createNotification');
-  await AwesomeNotifications().createNotification(
-    content: NotificationContent(
-      id: id,
-      channelKey: channelKey,
-      title: title,
-      body: body,
-    ),
-    schedule: NotificationCalendar.fromDate(
-      date: dateutil.parse(date).add(Duration(
-              days: days, hours: hours, minutes: minutes, seconds: seconds) *
-          -1),
-    ),
-  );
-}
+}) =>
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: id,
+        channelKey: channelKey,
+        title: title,
+        body: body,
+      ),
+      schedule: NotificationCalendar.fromDate(
+        date: dateutil.parse(date).add(Duration(
+                days: days, hours: hours, minutes: minutes, seconds: seconds) *
+            -1),
+      ),
+    );
